7de7e954fdd8cc13e7962944aec149c66b24ec69
LKYROS001 <70386625+LKYROS001@users.noreply.github.com>
2020-10-20 18:49:58 +0200

    Add files via upload

--- Reborn tempiboy.py
+++ Reborn tempiboy.py
@@ -4,20 +4,20 @@ import digitalio
 import board
 import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
+while True:
+    # create the spi bus
+    spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
 
-# create the spi bus
-spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
-
-# create the cs (chip select)
-cs = digitalio.DigitalInOut(board.D5)
+    # create the cs (chip select)
+    cs = digitalio.DigitalInOut(board.D5)
 
 
-# create the mcp object
-mcp = MCP.MCP3008(spi, cs)
+    # create the mcp object
+    mcp = MCP.MCP3008(spi, cs)
 
-# create an analog input channel on pin 0
-chan = AnalogIn(mcp, MCP.P0)
-temperature = chan.voltage / (10.0 / 1000)
-print("Raw ADC Value: ", chan.value)
-print("ADC Voltage: " + str(chan.voltage) + "V")
-print("Temp: " + str(temperature) + "C")
+    # create an analog input channel on pin 0
+    chan = AnalogIn(mcp, MCP.P0)
+    temperature = chan.voltage / (10.0 / 1000)
+    print("Raw ADC Value: ", chan.value)
+    print("ADC Voltage: " + str(chan.voltage) + "V")
+    print("Temp: " + str(temperature) + "C")

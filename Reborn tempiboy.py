b1354bb96f008f25d004b93bf5c98498406e31d0
LKYROS001 <70386625+LKYROS001@users.noreply.github.com>
2020-10-20 18:51:39 +0200

    Add files via upload

--- Reborn tempiboy.py
+++ Reborn tempiboy.py
@@ -4,6 +4,7 @@ import digitalio
 import board
 import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
+import time
 while True:
     # create the spi bus
     spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
@@ -20,4 +21,5 @@ while True:
     temperature = chan.voltage / (10.0 / 1000)
     print("Raw ADC Value: ", chan.value)
     print("ADC Voltage: " + str(chan.voltage) + "V")
-    print("Temp: " + str(temperature) + "C")
+    print("Temp: " + str(temperature) + "C")\
+    time.sleep(0.5)

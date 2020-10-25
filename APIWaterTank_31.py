6937dc6470e155fce049b16f26e6d3c9f243743f
LKYROS001 <70386625+LKYROS001@users.noreply.github.com>
2020-10-25 23:00:04 +0200

    Add files via upload

---
+++ APIWaterTank_31.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+import time
+
+Loading = ['\\____','/\___','_/\__','__/\_','___/\\','____/','_____','_____','_____','_____','_____','_____','_____','_____','_____','_____','_____']
+counter =0
+
+print("1 to view data, 2 to set tank depth, 3 to set minimum Temperature , 4 to set maximum Temperature,5 to monitor, 6 to quit")
+print("Enter a command:")
+print("5")
+print("press the w key to wake up")
+print("press CTRL+C to exit idle")
+print("idling")
+
+
+print((Loading[counter]))
+time.sleep(5)
+counter +=1
+print("2020/10/25, 14:52:16")
+print(("The water level is: 86"+"%"))
+print(("The ambient air temperatur is: 20.2"+chr(176)+"C. The water temperaturn is: 18.9"+chr(176)+"C."))
+
+print((Loading[counter]))
+time.sleep(5)
+counter +=1
+print("2020/10/25, 14:52:21")
+print(("The water level is: 72"+"%"))
+print(("The ambient air temperatur is: 20.1"+chr(176)+"C. The water temperaturn is: 19.1"+chr(176)+"C."))
+
+print((Loading[counter]))
+time.sleep(5)
+counter +=1
+print("2020/10/25, 14:52:26")
+print(("The water level is: 60"+"%"))
+print(("The ambient air temperatur is: 20.3"+chr(176)+"C. The water temperaturn is: 18.8"+chr(176)+"C."))
+
+print((Loading[counter]))
+time.sleep(5)
+counter +=1
+print("2020/10/25, 14:52:31")
+print(("The water level is: 49"+"%"))
+print(("The ambient air temperatur is: 20.4"+chr(176)+"C. The water temperaturn is: 19.0"+chr(176)+"C."))
+
+print((Loading[counter]))
+time.sleep(5)
+counter +=1
+print("2020/10/25, 14:52:36")
+print(("The water level is: 34"+"%"))
+print(("The ambient air temperatur is: 20.3"+chr(176)+"C. The water temperaturn is: 18.9"+chr(176)+"C."))
+
+print((Loading[counter]))
+time.sleep(5)
+counter +=1
+print("2020/10/25, 14:52:41")
+print(("The water level is: 22"+"%"))
+print(("The ambient air temperatur is: 20.1"+chr(176)+"C. The water temperaturn is: 18.9"+chr(176)+"C."))
+
+print((Loading[counter]))
+time.sleep(5)
+counter +=1
+print("2020/10/25, 14:52:46")
+print(("The water level is: 8"+"%"))
+print(("The ambient air temperatur is: 20.1"+chr(176)+"C. The water temperaturn is: 19.0"+chr(176)+"C."))
+
+print((Loading[counter]))
+time.sleep(5)
+counter +=1
+print("2020/10/25, 14:52:51")
+print(("The water level is: 6"+"%"))
+print(("The ambient air temperatur is: 20.0"+chr(176)+"C. The water temperaturn is: 19.1"+chr(176)+"C."))

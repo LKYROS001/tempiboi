5684f80aa45c0fb100e0bda32c8390fdc3cd3e02
LKYROS001 <70386625+LKYROS001@users.noreply.github.com>
2020-10-30 15:23:27 +0200

    Add files via upload

--- tempsense.py
+++ tempsense.py
@@ -27,6 +27,7 @@ def fetch_slave():
     
     time.sleep(timer)
 def main():
+    global starter
     # create the spi bus
     spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
     # create the cs (chip select)
